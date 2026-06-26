4d61fc67e1538cb6e873f789f449104fbef73067
Guadalupe Juárez Arias <lupita_ja_93@hotmail.com>
2026-06-26 17:23:39 -0600

    Agregar favicon Ruguex y ajustes finales Bobcat

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,6 +42,15 @@
 
 <meta name="description" content="@yield('meta_description', 'Compra y cotiza llantas para minicargadores Bobcat con asesoría Ruguex.')">
 
+<link rel="icon" href="{{ asset('favicon.ico') }}" sizes="any">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('icons/favicon-16x16.png') }}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('icons/favicon-32x32.png') }}">
+<link rel="icon" type="image/png" sizes="48x48" href="{{ asset('icons/favicon-48x48.png') }}">
+<link rel="icon" type="image/png" sizes="96x96" href="{{ asset('icons/favicon-96x96.png') }}">
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('icons/apple-touch-icon.png') }}">
+<link rel="manifest" href="{{ asset('site.webmanifest') }}">
+<meta name="theme-color" content="#ff6001">
+
 @if (config('app.noindex'))
     <meta name="robots" content="noindex,nofollow,noarchive,nosnippet">
 @else
